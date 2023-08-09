602e5a64730f02737994b90eceffd67cc7fb1e0e
venedictchen <venedictchen@gmail.com>
2023-08-09 17:44:41 +0700

    cleaning and fix new tab link

--- src/modules/ContactSection.tsx
+++ src/modules/ContactSection.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BiLogoInstagram, BiLogoGithub, BiLogoTwitter,BiLogoFacebookSquare } from 'react-icons/bi';
+import { BiLogoInstagram, BiLogoGithub, BiLogoTwitter, BiLogoFacebookSquare } from 'react-icons/bi';
 import { ImLinkedin } from 'react-icons/im';
 
 const ContactFooter = () => {
@@ -10,30 +10,30 @@ const ContactFooter = () => {
                     Connect</h3>
                 <div className="flex flex-wrap gap-10 py-10  md:ml-10 justify-center 
                 text-[#F29759] text-5xl">
-                <a href="https://github.com/venedictchen">
-                <BiLogoGithub className="transition duration-300 transform 
+                    <a href="https://github.com/venedictchen" target="_blank" rel="noopener noreferrer">
+                        <BiLogoGithub className="transition duration-300 transform 
                 hover:scale-125 hover:text-gray-300" />
-                </a>
-                <a href="https://www.linkedin.com/in/venedictchen/" 
-                >
-                <ImLinkedin className="transition duration-300 transform 
+                    </a>
+                    <a href="https://www.linkedin.com/in/venedictchen/"
+                        target="_blank" rel="noopener noreferrer">
+                        <ImLinkedin className="transition duration-300 transform 
                 hover:scale-125 hover:text-blue-600" />
-                </a>
-                <a href="https://www.linkedin.com/in/yourlinkedinprofile" 
-                >
-                <BiLogoInstagram className="transition duration-300 transform 
+                    </a>
+                    <a href="https://www.linkedin.com/in/yourlinkedinprofile"
+                        target="_blank" rel="noopener noreferrer">
+                        <BiLogoInstagram className="transition duration-300 transform 
                 hover:scale-125 hover:text-yellow-500" />
-                </a>
-                <a href="https://twitter.com/theonlyvenee" 
-                >
-                <BiLogoTwitter className="transition duration-300 transform 
+                    </a>
+                    <a href="https://twitter.com/theonlyvenee"
+                        target="_blank" rel="noopener noreferrer">
+                        <BiLogoTwitter className="transition duration-300 transform 
                 hover:scale-125 hover:text-blue-500" />
-                </a>
-                <a href="https://www.facebook.com/venedictchen.venedictchen.1/" 
-                >
-                <BiLogoFacebookSquare className="transition duration-300 transform 
+                    </a>
+                    <a href="https://www.facebook.com/venedictchen.venedictchen.1/"
+                        target="_blank" rel="noopener noreferrer">
+                        <BiLogoFacebookSquare className="transition duration-300 transform 
                 hover:scale-125 hover:text-blue-700" />
-                </a>
+                    </a>
                 </div>
             </div>
         </footer>
